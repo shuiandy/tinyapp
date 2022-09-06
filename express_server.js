79f8969de6dbdafe473742e63f51f814a791df9a
Shuai Hu <shuiandy@gmail.com>
2022-09-06 01:15:43 -0400

    main branch

--- express_server.js
+++ express_server.js
@@ -3,13 +3,63 @@ const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
 };
 
+const generateRandomString = () => {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charLen = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charLen));
+  }
+  return result;
+};
+
 app.get('/', (req, res) => {
   res.send('Hello!');
+});
+
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+app.post('/urls', (req, res) => {
+  const stringKey = generateRandomString();
+  console.log(stringKey);
+  urlDatabase[stringKey] = req.body.longURL;
+  res.redirect('http://localhost:8080/urls');
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.get('/urls/:id', (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render('urls_show', templateVars);
+});
+app.post('/urls/:id', (req, res) => {
+  console.log('here');
+  res.redirect(`http://localhost:8080/urls/${req.params.id}`);
+});
+
+app.get('/u/:id', (req, res) => {
+  res.redirect(urlDatabase[req.params.id]);
+});
+app.post('/urls/:id/edit', (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+  res.redirect('http://localhost:8080/urls');
+});
+
+app.post('/urls/:id/delete', (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect('http://localhost:8080/urls');
 });
 
 app.listen(PORT, () => {
